97d536ca034c8fc9c405bc4dcc0940f9a43f7a8f
p6oissi <markus.tiedemann.mt@gmail.com>
2026-02-28 18:36:51 +0100

    Initial project setup

---
+++ src/main/java/com/cgi/table4u/Table4UApplication.java
@@ -0,0 +1,13 @@
+package com.cgi.table4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Table4UApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Table4UApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cgi/table4u/Table4UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.table4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Table4UApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
